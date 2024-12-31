a0350cbb66dddcafd24b400f0ff81a0fd9b39fb7
kusoroadeolu <kusoroadeolu@gmail.com>
2024-12-31 21:07:02 +0100

    Removed useless code from the App class

--- src/App.java
+++ src/App.java
@@ -1,13 +1,5 @@
 public class App {
-
-    private static int ClassId = 0;
-    private String name;
-
-    public App(String name) {
-        this.name = name + " " + ClassId;
-    }
-
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
         // iniitalize
         LaunchTracker tr = new LaunchTracker();
         tr.launchTracker(args);
